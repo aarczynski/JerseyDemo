a6808e0524e6a6b8be57b6ce74710d8110e95dfc
Karister <adam.arczynski@gmail.com>
2016-07-25 00:31:42 +0200

    refactor

--- src/main/java/pl/arczynskiadam/jersey/exception/MyIllegalArgumentException.java
+++ src/main/java/pl/arczynskiadam/jersey/exception/MyIllegalArgumentException.java
@@ -6,8 +6,15 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+
 public class MyIllegalArgumentException extends WebApplicationException {
     public MyIllegalArgumentException(String msg) {
-        super(Response.status(400).entity(new ErrorDTO(msg, "400")).type(MediaType.APPLICATION_JSON).build());
+        super(Response
+                .status(BAD_REQUEST)
+                .entity(new ErrorDTO(msg, Integer.toString(BAD_REQUEST.getStatusCode())))
+                .type(MediaType.APPLICATION_JSON)
+                .build()
+        );
     }
 }
